b903c1a770c1c15989f54353b8d5b8608a43374e
vellimir <kkazancov@gmail.com>
2024-02-27 00:22:38 +0300

    add users, get users

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 
 from src.api.routers.user import router as user_router
+from src.database.database import create_tables, delete_tables
 
 
 app = FastAPI()
@@ -10,8 +11,8 @@ app = FastAPI()
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # await delete_tables()
-    print('БД очищена')
-#     await create_tables()
+    # print('БД очищена')
+    await create_tables()
     print('БД готова')
     yield
     print('выключение')
